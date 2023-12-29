b76ba21a0b49352e6e0c162e3ebaa527941dd452
Vladislav Kozlov <vlad.kv.2002@gmail.com>
2023-12-28 22:14:39 -0700

    HW-02

--- homework_02/base.py
+++ homework_02/base.py
@@ -1,5 +1,60 @@
+import numbers
 from abc import ABC
-
+from homework_02.exceptions import (
+    LowFuelError,
+    NotEnoughFuel,
+)
 
 class Vehicle(ABC):
-    pass
+    def __init__(self, weight=0, fuel=0, fuel_consumption=0):
+        self._weight = self._validate_int_value(weight)
+        self._fuel = self._validate_int_value(fuel)
+        self._fuel_consumption = self._validate_int_value(fuel_consumption)
+        self._started = False
+
+    def _validate_int_value(self, value):
+        if isinstance(value, numbers.Integral):
+            return int(value)
+        raise TypeError("number is not integral")
+
+    @property
+    def weight(self):
+        return self._weight
+
+    @weight.setter
+    def weight(self, value):
+        self._weight = self._validate_int_value(value)
+
+    @property
+    def fuel(self):
+        return self._fuel
+
+    @fuel.setter
+    def fuel(self, value):
+        self._fuel = self._validate_int_value(value)
+
+    @property
+    def fuel_consumption(self):
+        return self._fuel_consumption
+
+    @fuel_consumption.setter
+    def fuel_consumption(self, value):
+        self._fuel_consumption = self._validate_int_value(value)
+
+    @property
+    def started(self):
+        return self._started
+
+    @started.setter
+    def started(self, value):
+        self._started = bool(value)
+
+    def start(self):
+        if not self._started and self._fuel <= 0:
+            raise LowFuelError("low on fuel")
+        self._started = True
+
+    def move(self, distance):
+        if distance * self._fuel_consumption > self._fuel:
+            raise NotEnoughFuel
+        self._fuel -= distance * self._fuel_consumption
--- homework_02/plane.py
+++ homework_02/plane.py
@@ -1,3 +1,41 @@
 """
 создайте класс `Plane`, наследник `Vehicle`
 """
+
+from homework_02.base import Vehicle
+from homework_02.exceptions import CargoOverload
+
+class Plane(Vehicle):
+    _cargo: int = 0
+    _max_cargo: int = 0
+
+    def __init__(self, weight, fuel, fuel_consumption, max_cargo):
+        super().__init__(weight, fuel, fuel_consumption)
+        self._max_cargo = max_cargo
+
+    @property
+    def cargo(self):
+        return self._cargo
+
+    @cargo.setter
+    def cargo(self, value):
+        self._cargo = value
+
+    @property
+    def max_cargo(self):
+        return self._max_cargo
+
+    @max_cargo.setter
+    def max_cargo(self, value):
+        self._max_cargo = value
+
+    def load_cargo(self, value: int):
+        if self._cargo + value > self._max_cargo:
+            raise CargoOverload
+        self._cargo += value
+
+    def remove_all_cargo(self):
+        value = self._cargo
+        self._cargo = 0
+        return value
+
