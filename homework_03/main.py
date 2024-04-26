954c6337cd66e84dab0da263b2f9386db290ffc3
Vladislav Kozlov <vlad.kv.2002@gmail.com>
2024-04-25 22:37:10 -0600

    HW04: revert HW03

--- homework_03/main.py
+++ homework_03/main.py
@@ -1,15 +1,19 @@
 from fastapi import FastAPI
 
-app=FastAPI()
+app = FastAPI()
+
 
 @app.get("/")
 def root():
     return {"message": "Hello world!"}
 
+
 @app.get("/ping/")
 def ping():
     return {"message": "pong"}
 
+
 @app.get("/greet/")
 def make_greeting(name: str = "Guest"):
     return {"message": f"Hello {name}!"}
+
